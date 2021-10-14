9596a43043efe0179c2b9f9c36799cf6eb39e160
bdairshadi <bdairshadi@gmail.com>
2021-10-15 01:38:58 +0300

    Added the variable: environment

---
+++ generic-input-variables.tf
@@ -0,0 +1,9 @@
+# Author Shadi Badir
+
+# Generic Input Variables
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "preprod"
+}
