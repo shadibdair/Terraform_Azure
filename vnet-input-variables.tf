6722144a97cf8c72c566b96eff5d669c43a3a883
bdairshadi <bdairshadi@gmail.com>
2021-10-15 02:09:46 +0300

    Added the variable: db subnet name & db subnet address

--- vnet-input-variables.tf
+++ vnet-input-variables.tf
@@ -27,3 +27,16 @@ variable "web_subnet_address" {
   type = list(string)
   default = ["10.0.1.0/24"]
 }
+
+# Database Subnet Name
+variable "db_subnet_name" {
+  description = "Virtual Network Database Subnet Name"
+  type = string
+  default = "dbsubnet"
+}
+# Database Subnet Address Space
+variable "db_subnet_address" {
+  description = "Virtual Network Database Subnet Address Spaces"
+  type = list(string)
+  default = ["10.0.21.0/24"]
+}
