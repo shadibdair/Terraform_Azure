cd4f01e70d957f6e35970fe115a61a4d6f69586a
bdairshadi <bdairshadi@gmail.com>
2021-10-15 02:05:57 +0300

    Added the variable: vnet name

---
+++ vnet-input-variables.tf
@@ -0,0 +1,10 @@
+# Author Shadi Badir
+
+# Virtual Network, Subnets and Subnet NSG's
+
+## Virtual Network
+variable "vnet_name" {
+  description = "Virtual Network name"
+  type = string
+  default = "vnet"
+}
