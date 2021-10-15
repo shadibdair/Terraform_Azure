475a7b87f9213669ff90ab666bde9ed24786898d
bdairshadi <bdairshadi@gmail.com>
2021-10-15 15:51:54 +0300

    Added the resource db NSG

--- db-subnet-and-nsg.tf
+++ db-subnet-and-nsg.tf
@@ -7,3 +7,10 @@ resource "azurerm_subnet" "dbsubnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.db_subnet_address  
 }
+
+# Resource-2: Create Network Security Group (NSG)
+resource "azurerm_network_security_group" "db_subnet_nsg" {
+  name                = "${azurerm_subnet.dbsubnet.name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
