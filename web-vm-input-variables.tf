3fdd9d70b05375a0ee61fd82fb6c55f6abf04ea1
bdairshadi <bdairshadi@gmail.com>
2021-10-15 16:20:01 +0300

    Added the web linux VM instance count

---
+++ web-vm-input-variables.tf
@@ -0,0 +1,10 @@
+# Author Shadi Badir
+
+
+# Linux VM Input Variables Placeholder file.
+# Web Linux VM Instance Count
+variable "web_linuxvm_instance_count" {
+  description = "Web Linux VM Instance Count"
+  type = number 
+  default = 1
+}
