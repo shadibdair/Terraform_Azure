6aa22f5d5277185d1f8ced805df1d026040e05b8
bdairshadi <bdairshadi@gmail.com>
2021-10-15 16:21:17 +0300

    Added the web LB inbout NAT port for all VMs

--- web-vm-input-variables.tf
+++ web-vm-input-variables.tf
@@ -8,3 +8,10 @@ variable "web_linuxvm_instance_count" {
   type = number 
   default = 1
 }
+
+# Web LB Inbout NAT Port for All VMs
+variable "lb_inbound_nat_ports" {
+  description = "Web LB Inbound NAT Ports List"
+  type = list(string)
+  default = ["8080", "8080", "8080"]
+}
