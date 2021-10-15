c50fa327388a7cf80bcce15772669ab4c7b6a6e0
bdairshadi <bdairshadi@gmail.com>
2021-10-15 15:41:57 +0300

    Added the resource web NSG inbound rule

--- web-subnet-and-nsg.tf
+++ web-subnet-and-nsg.tf
@@ -29,3 +29,20 @@ locals {
     "3010" : "22"
   } 
 }
+
+
+## NSG Inbound Rule for WebTier Subnets
+resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
+  for_each = local.web_inbound_ports_map
+  name                        = "Rule-Port-${each.value}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value 
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.web_subnet_nsg.name
+}
