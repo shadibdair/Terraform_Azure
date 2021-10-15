f31122c20229c4265ecbe2663130814c558298de
bdairshadi <bdairshadi@gmail.com>
2021-10-16 01:56:48 +0300

    Added the public IP address for azure load balancer

---
+++ web-loadbalancer-resource.tf
@@ -0,0 +1,12 @@
+# Author Shadi Badir
+
+
+# Resource-1: Create Public IP Address for Azure Load Balancer
+resource "azurerm_public_ip" "web_lbpublicip" {
+  name                = "${local.resource_name_prefix}-lbpublicip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+  sku = "Standard"
+  tags = local.common_tags
+}
