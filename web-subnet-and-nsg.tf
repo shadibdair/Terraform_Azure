7992d238bc364a388f44a6bc7cdb6f56d72eed48
bdairshadi <bdairshadi@gmail.com>
2021-10-15 15:31:18 +0300

    Added the resource web subnet

---
+++ web-subnet-and-nsg.tf
@@ -0,0 +1,10 @@
+# Resource 1: Create WebTier Subnet
+resource "azurerm_subnet" "websubnet" {
+  name                 = "${azurerm_virtual_network.vnet.name}-${var.web_subnet_name}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.web_subnet_address  
+}
+
+
+
