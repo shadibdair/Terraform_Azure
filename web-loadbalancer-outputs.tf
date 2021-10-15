c1b8bb9babcee38fb12d8ff50d11faaa49c50e39
bdairshadi <bdairshadi@gmail.com>
2021-10-16 02:11:16 +0300

    Added the output LB id

--- web-loadbalancer-outputs.tf
+++ web-loadbalancer-outputs.tf
@@ -6,3 +6,9 @@ output "web_lb_public_ip_address" {
   description = "Web Load Balancer Public Address"
   value = azurerm_public_ip.web_lbpublicip.ip_address
 }
+
+# Load Balancer ID
+output "web_lb_id" {
+  description = "Web Load Balancer ID"
+  value = azurerm_lb.web_lb.id 
+}
