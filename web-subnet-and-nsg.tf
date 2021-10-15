fc8c69b05f1669bad9adfcb4041da095ffddb5a1
bdairshadi <bdairshadi@gmail.com>
2021-10-15 15:32:30 +0300

    Added the resource web network security group

--- web-subnet-and-nsg.tf
+++ web-subnet-and-nsg.tf
@@ -6,5 +6,10 @@ resource "azurerm_subnet" "websubnet" {
   address_prefixes     = var.web_subnet_address  
 }
 
-
+# Resource 2: Create Network Security Group (NSG)
+resource "azurerm_network_security_group" "web_subnet_nsg" {
+  name                = "${azurerm_subnet.websubnet.name}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
