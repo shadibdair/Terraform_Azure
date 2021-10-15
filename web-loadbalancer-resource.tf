a61e7b5e258a388bb1d1daf3508a7c77cdaf6882
bdairshadi <bdairshadi@gmail.com>
2021-10-16 01:58:13 +0300

    Added the standard load balancer

--- web-loadbalancer-resource.tf
+++ web-loadbalancer-resource.tf
@@ -10,3 +10,15 @@ resource "azurerm_public_ip" "web_lbpublicip" {
   sku = "Standard"
   tags = local.common_tags
 }
+
+# Resource-2: Create Azure Standard Load Balancer
+resource "azurerm_lb" "web_lb" {
+  name                = "${local.resource_name_prefix}-web-lb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku = "Standard"
+  frontend_ip_configuration {
+    name                 = "web-lb-publicip-1"
+    public_ip_address_id = azurerm_public_ip.web_lbpublicip.id
+  }
+}
