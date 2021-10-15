06f0cf45b7686db4aeaa7dc91ae087250f8473a7
bdairshadi <bdairshadi@gmail.com>
2021-10-15 15:56:22 +0300

    Added the resource db NSG rule inbound

--- db-subnet-and-nsg.tf
+++ db-subnet-and-nsg.tf
@@ -31,3 +31,19 @@ locals {
     "3010" : "22"
   } 
 }
+
+## NSG Inbound Rule for DBTier Subnets
+resource "azurerm_network_security_rule" "db_nsg_rule_inbound" {
+  for_each = local.db_inbound_ports_map
+  name                        = "Rule-Port-${each.value}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value 
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.db_subnet_nsg.name
+}
