9fa2d2dcd56062884828de1ccc73ceef0c028d98
bdairshadi <bdairshadi@gmail.com>
2021-10-16 02:12:04 +0300

    Added the output load balancer frontend IP configuration block

--- web-loadbalancer-outputs.tf
+++ web-loadbalancer-outputs.tf
@@ -12,3 +12,9 @@ output "web_lb_id" {
   description = "Web Load Balancer ID"
   value = azurerm_lb.web_lb.id 
 }
+
+# Load Balancer Frontend IP Configuration Block
+output "web_lb_frontend_ip_configuration" {
+  description = "Web LB frontend_ip_configuration Block"
+  value = [azurerm_lb.web_lb.frontend_ip_configuration]
+}
