62a0011e3d259164ece06cb9d484b8070cbfe225
bdairshadi <bdairshadi@gmail.com>
2021-10-15 15:52:56 +0300

    Added the resource db subnet NSG association

--- db-subnet-and-nsg.tf
+++ db-subnet-and-nsg.tf
@@ -14,3 +14,10 @@ resource "azurerm_network_security_group" "db_subnet_nsg" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+# Resource-3: Associate NSG and Subnet
+resource "azurerm_subnet_network_security_group_association" "db_subnet_nsg_associate" {
+  depends_on = [ azurerm_network_security_rule.db_nsg_rule_inbound]  
+  subnet_id                 = azurerm_subnet.dbsubnet.id
+  network_security_group_id = azurerm_network_security_group.db_subnet_nsg.id
+}
