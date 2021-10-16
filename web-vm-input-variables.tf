5768ed80181dcf92aeabdff173b867551cc74d18
bdairshadi <bdairshadi@gmail.com>
2021-10-16 03:14:02 +0300

    Edited the web vm isntances using map

--- web-vm-input-variables.tf
+++ web-vm-input-variables.tf
@@ -5,13 +5,9 @@
 # Web Linux VM Instance Count
 variable "web_linuxvm_instance_count" {
   description = "Web Linux VM Instance Count"
-  type = number 
-  default = 1
-}
-
-# Web LB Inbout NAT Port for All VMs
-variable "lb_inbound_nat_ports" {
-  description = "Web LB Inbound NAT Ports List"
-  type = list(string)
-  default = ["8080", "8080", "8080"]
+  type = map(string)
+  default = {
+    "vm1" = "1022",
+    "vm2" = "2022"
+  }
 }
