c387fac6d619484da54dbb7c80b17ba2ce856f75
bdairshadi <bdairshadi@gmail.com>
2021-10-17 02:19:41 +0300

    Fixed some bugs & Edited

--- web-loadbalancer-resource.tf
+++ web-loadbalancer-resource.tf
@@ -6,7 +6,7 @@ resource "azurerm_public_ip" "web_lbpublicip" {
   name                = "${local.resource_name_prefix}-lbpublicip"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku = "Standard"
   tags = local.common_tags
 }
