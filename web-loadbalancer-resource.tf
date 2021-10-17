291808c342985622710e8286732579a4d7a4bcf4
bdairshadi <bdairshadi@gmail.com>
2021-10-17 05:43:34 +0300

    Edited inside some resources

--- web-loadbalancer-resource.tf
+++ web-loadbalancer-resource.tf
@@ -3,7 +3,7 @@
 
 # Resource-1: Create Public IP Address for Azure Load Balancer
 resource "azurerm_public_ip" "web_lbpublicip" {
-  name                = "${local.resource_name_prefix}-lbpublicip"
+  name                = "${local.resource_name_prefix}-lbpublicip-bonus"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Static"
@@ -13,25 +13,25 @@ resource "azurerm_public_ip" "web_lbpublicip" {
 
 # Resource-2: Create Azure Standard Load Balancer
 resource "azurerm_lb" "web_lb" {
-  name                = "${local.resource_name_prefix}-web-lb"
+  name                = "${local.resource_name_prefix}-web-lb-bonus"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku = "Standard"
   frontend_ip_configuration {
-    name                 = "web-lb-publicip-1"
+    name                 = "web-lb-publicip-1-bonus"
     public_ip_address_id = azurerm_public_ip.web_lbpublicip.id
   }
 }
 
 # Resource-3: Create LB Backend Pool
 resource "azurerm_lb_backend_address_pool" "web_lb_backend_address_pool" {
-  name                = "web-backend"
+  name                = "web-backend-bonus"
   loadbalancer_id     = azurerm_lb.web_lb.id
 }
 
 # Resource-4: Create LB Probe
 resource "azurerm_lb_probe" "web_lb_probe" {
-  name                = "tcp-probe"
+  name                = "tcp-probe-bonus"
   protocol            = "Tcp"
   port                = 8080
   loadbalancer_id     = azurerm_lb.web_lb.id
@@ -40,7 +40,7 @@ resource "azurerm_lb_probe" "web_lb_probe" {
 
 # Resource-5: Create LB Rule
 resource "azurerm_lb_rule" "web_lb_rule_app1" {
-  name                           = "web-app1-rule"
+  name                           = "web-app1-rule-bonus"
   protocol                       = "Tcp"
   frontend_port                  = 8080
   backend_port                   = 8080
@@ -51,10 +51,10 @@ resource "azurerm_lb_rule" "web_lb_rule_app1" {
   resource_group_name            = azurerm_resource_group.rg.name
 }
 
-# Resource-6: Associate Network Interface and Standard Load Balancer
-resource "azurerm_network_interface_backend_address_pool_association" "web_nic_lb_associate" {
-  for_each = var.web_linuxvm_instance_count
-  network_interface_id    = azurerm_network_interface.web_linuxvm_nic[each.key].id
-  ip_configuration_name   = azurerm_network_interface.web_linuxvm_nic[each.key].ip_configuration[0].name
-  backend_address_pool_id = azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id
+# # Resource-6: Associate Network Interface and Standard Load Balancer
+# resource "azurerm_network_interface_backend_address_pool_association" "web_nic_lb_associate" {
+#   for_each = var.web_linuxvm_instance_count
+#   network_interface_id    = azurerm_network_interface.web_linuxvm_nic[each.key].id
+#   ip_configuration_name   = azurerm_network_interface.web_linuxvm_nic[each.key].ip_configuration[0].name
+#   backend_address_pool_id = azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id
 }
