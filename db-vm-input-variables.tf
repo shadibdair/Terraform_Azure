e21864465c5760f1ab7ac49f5ef113f89a337a25
bdairshadi <bdairshadi@gmail.com>
2021-10-17 15:36:33 +0300

    Added the input variables

---
+++ db-vm-input-variables.tf
@@ -0,0 +1,27 @@
+# Author Shadi Badir
+
+
+# Input Variables
+# DB Name
+variable "mysql_db_name" {
+  description = "Azure MySQL Database Name"
+  type        = string
+}
+
+# DB Username
+variable "mysql_db_username" {
+  description = "Azure MySQL Database Administrator Username"
+  type        = string
+}
+# DB Password - Enable Sensitive flag
+variable "mysql_db_password" {
+  description = "Azure MySQL Database Administrator Password"
+  type        = string
+  sensitive   = true
+}
+
+# DB Schema Name
+variable "mysql_db_schema" {
+  description = "Azure MySQL Database Schema Name"
+  type        = string
+}
