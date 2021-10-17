e02ac5544f4ff21412354bd12529753cae28a6f1
bdairshadi <bdairshadi@gmail.com>
2021-10-17 15:34:48 +0300

    Added the var.resource_group_location inside the perfix name

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@
 locals {
   owners = var.author
   environment = var.environment
-  resource_name_prefix = "${var.author}-${var.environment}"
+  resource_name_prefix = "${var.resource_group_location}-${var.author}-${var.environment}"
   common_tags = {
     owners = local.owners
     environment = local.environment
