c99bafa0fe9c4be2ad46cefedf96e2ce4ec9ed8f
bdairshadi <bdairshadi@gmail.com>
2021-10-17 18:52:18 +0300

    Commited the variables per test

--- db-vm-input-variables.tf
+++ db-vm-input-variables.tf
@@ -9,19 +9,19 @@ variable "mysql_db_name" {
 }
 
 # DB Username
-variable "mysql_db_username" {
-  description = "Azure MySQL Database Administrator Username"
-  type        = string
-}
-# DB Password - Enable Sensitive flag
-variable "mysql_db_password" {
-  description = "Azure MySQL Database Administrator Password"
-  type        = string
-  sensitive   = true
-}
+# variable "mysql_db_username" {
+#   description = "Azure MySQL Database Administrator Username"
+#   type        = string
+# }
+# # DB Password - Enable Sensitive flag
+# variable "mysql_db_password" {
+#   description = "Azure MySQL Database Administrator Password"
+#   type        = string
+#   sensitive   = true
+# }
 
-# DB Schema Name
-variable "mysql_db_schema" {
-  description = "Azure MySQL Database Schema Name"
-  type        = string
-}
+# # DB Schema Name
+# variable "mysql_db_schema" {
+#   description = "Azure MySQL Database Schema Name"
+#   type        = string
+# }
