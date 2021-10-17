6a84dbcdcb6c2799a260daf7de802392ec11e370
bdairshadi <bdairshadi@gmail.com>
2021-10-17 15:32:48 +0300

    Edited the name of the NIC

--- db-vm-network-interface.tf
+++ db-vm-network-interface.tf
@@ -1,13 +1,13 @@
 # Author Shadi Badir
 
 # Resource-2: Create Network Interface
-resource "azurerm_network_interface" "db_linuxvm_nic" {
-  name                = "${local.resource_name_prefix}-db-linuxvm-nic"
+resource "azurerm_network_interface" "db_postgress_nic" {
+  name                = "${local.resource_name_prefix}-db_postgress_nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "db-linuxvm-ip-1"
+    name                          = "db-postgress-ip-1"
     subnet_id                     = azurerm_subnet.dbsubnet.id
     private_ip_address_allocation = "Dynamic"
   }
