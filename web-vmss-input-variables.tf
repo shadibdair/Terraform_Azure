b5328c75b7bebe22638f227ed7e20bd1aac36d0e
bdairshadi <bdairshadi@gmail.com>
2021-10-17 05:15:22 +0300

    Added a variable web vmss nsg inbound ports

---
+++ web-vmss-input-variables.tf
@@ -0,0 +1,9 @@
+# Author Shadi Badir
+
+
+# Linux VM Input Variables Placeholder file.
+variable "web_vmss_nsg_inbound_ports" {
+  description = "Web VMSS NSG Inbound Ports"
+  type = list(string)
+  default = [22, 8080]
+}
