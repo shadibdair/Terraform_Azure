66a71555dd4c935130dc426ba0b4f7ffee2b6b71
bdairshadi <bdairshadi@gmail.com>
2021-10-17 05:02:31 +0300

    Edited the resource group name:bonus

--- resource-group.tf
+++ resource-group.tf
@@ -2,7 +2,7 @@
 
 # Resource: Azure Resource Group
 resource "azurerm_resource_group" "rg" {
-  name = "${local.resource_name_prefix}-${var.resource_group_name}-basic"
+  name = "${local.resource_name_prefix}-${var.resource_group_name}-bonus"
   location = var.resource_group_location
   tags = local.common_tags
 }
