b3f8383917f8da00d3dce3eaf0b3f373ebfdd421
bdairshadi <bdairshadi@gmail.com>
2021-10-19 03:11:08 +0300

    Added another secret file

--- db-vm-input-variables.tf
+++ db-vm-input-variables.tf
@@ -1,27 +1,28 @@
 # Author Shadi Badir
 
+# For TEST
 
 # Input Variables
 # DB Name
-variable "mysql_db_name" {
-  description = "Azure MySQL Database Name"
-  type        = string
-}
+# variable "postgress_db_name" {
+#   description = "Azure MySQL Database Name"
+#   type        = string
+# }
 
-# DB Username
-# variable "mysql_db_username" {
+# # DB Username
+# variable "postgress_db_username" {
 #   description = "Azure MySQL Database Administrator Username"
 #   type        = string
 # }
 # # DB Password - Enable Sensitive flag
-# variable "mysql_db_password" {
+# variable "postgress_db_password" {
 #   description = "Azure MySQL Database Administrator Password"
 #   type        = string
 #   sensitive   = true
 # }
 
 # # DB Schema Name
-# variable "mysql_db_schema" {
+# variable "postgress_db_schema" {
 #   description = "Azure MySQL Database Schema Name"
 #   type        = string
 # }
