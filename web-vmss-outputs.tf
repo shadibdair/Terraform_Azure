4c0553078491453f8094f18e0bc107d766db5bda
bdairshadi <bdairshadi@gmail.com>
2021-10-19 03:28:23 +0300

    Added two outputs for test

--- web-vmss-outputs.tf
+++ web-vmss-outputs.tf
@@ -5,3 +5,16 @@ output "web_vmss_id" {
   description = "Web Virtual Machine Scale Set ID"
   value = azurerm_linux_virtual_machine_scale_set.web_linuxvm.id 
 }
+
+# Output admin of the web VM
+output "information_Admin" {
+  description = "Web Linux VM Admin"
+  value = azurerm_linux_virtual_machine_scale_set.web_linuxvm.admin_username
+}
+
+# Output password of the web VM
+output "information_Password" {
+  description = "Web Linux VM password"
+  sensitive   = true
+  value = azurerm_linux_virtual_machine_scale_set.web_linuxvm.admin_password
+}
